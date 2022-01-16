9fba55eb4751605e06f4a0bb7c7fcfde47c80bc8
Douglas Lovera <doulovera@gmail.com>
2022-01-16 12:59:34 -0500

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://doulovera.me/',
+  base: 'https://doulovera.github.io/',
   plugins: [react()]
 })
